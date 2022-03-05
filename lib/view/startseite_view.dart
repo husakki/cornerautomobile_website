86676cb042e7fa12fb9f32e633bbe66e50c4a34d
husakki <hueschromo@gmail.com>
2022-03-05 12:44:42 +0100

    init dart files

---
+++ lib/view/startseite_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartSeiteView extends StatelessWidget {
+  const StartSeiteView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
