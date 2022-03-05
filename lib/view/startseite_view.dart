0d26e7c149578004dda7fbcc8e5dd8b1f59afbc6
husakki <hueschromo@gmail.com>
2022-03-05 13:46:02 +0100

    added images | added mobx package | starting with the homescreen

--- lib/view/startseite_view.dart
+++ lib/view/startseite_view.dart
@@ -5,6 +5,8 @@ class StartSeiteView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Text("okay"),
+    );
   }
 }
