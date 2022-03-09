50a6c48f50d353f2c9fd76d67d60141b68509b66
husakki <hueschromo@gmail.com>
2022-03-09 15:20:57 +0100

    added temporary theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,13 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Corner Automobile',
-      home: HomePageView(),
+      home: const HomePageView(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
     );
   }
 }
