0ec49fe9735d73aaf623dac3a2713feadd92e00d
husakki <hueschromo@gmail.com>
2022-03-09 10:48:47 +0100

    added carousel packg with working carousel

--- lib/view/homepage/homepage_view.dart
+++ lib/view/homepage/homepage_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cornerautomobile_website/common/views/skeleton_view.dart';
 import 'package:flutter/material.dart';
 
@@ -9,37 +10,45 @@ class HomePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SkeletonView(
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Image.asset(
-            'assets/images/sampleImage.png',
-            height: 300,
-          ),
-          const Divider(
-            height: 50,
-          ),
-          Center(
-            child: Container(
-              child: const Align(
-                alignment: Alignment.center,
-                child: Text(
-                    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
-              ),
-              width: 600,
-              height: 200,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey.shade900),
-                color: Colors.grey,
-              ),
+        body: ListView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Image.asset(
+          'assets/images/sampleImage.png',
+          height: 300,
+        ),
+        const Divider(
+          height: 50,
+        ),
+        Center(
+          child: Container(
+            child: const Align(
+              alignment: Alignment.center,
+              child: Text(
+                  "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
+            ),
+            width: 600,
+            height: 200,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade900),
+              color: Colors.grey,
             ),
           ),
-          const Divider(
-            height: 50,
-          ),
-          CarCard(),
-        ],
-      ),
-    );
+        ),
+        const Divider(
+          height: 50,
+        ),
+        CarouselSlider(
+          options: CarouselOptions(height: 400.0),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return const CarCard();
+              },
+            );
+          }).toList(),
+        )
+      ],
+    ));
   }
 }
