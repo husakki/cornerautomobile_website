1bcfef1bd69055bc0cf5e02caa7a39615b120344
husakki <hueschromo@gmail.com>
2022-03-10 10:39:27 +0100

    added button and text in scaffold

--- lib/common/views/skeleton_view.dart
+++ lib/common/views/skeleton_view.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
-
 class SkeletonView extends StatelessWidget {
   Widget? _body;
-  SkeletonView({Key? key, required Widget body}) : super(key: key){
+  SkeletonView({Key? key, required Widget body}) : super(key: key) {
     _body = body;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Corner Automobile"),
+        leading: IconButton(
+          onPressed: (() => {}),
+          icon: const Icon(Icons.menu),
+        ),
+      ),
       body: _body,
     );
   }
