052ea4fdd759c18a5309538c96fe76513d4a0a50
husakki <hueschromo@gmail.com>
2022-03-14 15:10:19 +0100

    added button Color and color variables

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,35 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-var mainTheme = ThemeData(
-    colorScheme: const ColorScheme(
-      brightness: Brightness.light,
-      primary: Color.fromARGB(255, 141, 22, 48),
-      onPrimary: Colors.black,
-      secondary: Color.fromARGB(255, 109, 177, 191),
-      onSecondary: Color.fromARGB(255, 109, 177, 191),
-      error: Colors.red,
-      onError: Colors.red,
-      background: Colors.black,
-      onBackground: Colors.black,
-      surface: Color(0xFFFFEAEC),
-      onSurface: Color(0xFFFFEAEC),
-    ),
-    fontFamily: GoogleFonts.lato().fontFamily,
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.resolveWith(getColor),
-      ),
-    ));
+Color _mainC = const Color.fromARGB(255, 141, 22, 48);
+Color _secondaryC = const Color.fromARGB(255, 109, 177, 191);
+Color _thirdC = const Color(0xFFFFEAEC);
 
-Color getColor(Set<MaterialState> states) {
-  const Set<MaterialState> interactiveStates = <MaterialState>{
-    MaterialState.pressed,
-    MaterialState.hovered,
-    MaterialState.focused,
-  };
-  if (states.any(interactiveStates.contains)) {
-    return Colors.blue;
-  }
-  return Colors.red;
-}
+var mainTheme = ThemeData(
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: _mainC,
+    onPrimary: Colors.black,
+    secondary: _secondaryC,
+    onSecondary: _secondaryC,
+    error: Colors.red,
+    onError: Colors.red,
+    background: Colors.black,
+    onBackground: Colors.black,
+    surface: Colors.orange,
+    onSurface: Colors.orange,
+  ),
+  fontFamily: GoogleFonts.lato().fontFamily,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(
+        _secondaryC,
+      ),
+    ),
+  ),
+  cardColor: _thirdC,
+);
